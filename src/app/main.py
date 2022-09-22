457a1fa711268df8fd41374a074baf95ba699324
Nithya Natarajan <infonithiya@gmail.com>
2022-09-22 10:00:31 -0700

    implemented SW-6

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 print(
     """============================================================================================================
 
@@ -8,11 +10,18 @@ Menu:
 [1] Add stars       [2] Register a new transaction    [3] View transactions    [4] Summary"""
 )
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="admin",
+    database="mysql"
+)
+
 
 def menu():
     user_input = int(input('Enter your choice: '))
     if user_input == 1:
-        add_stars()
+        add_stars(account_id=1234, stars=30)
 
     elif user_input == 2:
         register_a_new_transaction()
@@ -29,8 +38,20 @@ def menu():
     menu()
 
 
-def add_stars():
-    print('The add stars functionality will be added in this function')
+def add_stars_to_account(account_id, add_count):
+    cursor = mydb.cursor()
+    update_table = f"update stars.account set stars = stars + {add_count} where account_id = '{account_id}'"
+    no_of_records_updated = cursor.execute(update_table)
+    mydb.commit()
+    if no_of_records_updated == 1:
+        return True
+    else:
+        return False
+
+
+def add_stars(account_id, stars):
+    total_stars = (f'{stars} + {new_stars}')
+    return total_stars
 
 
 def register_a_new_transaction():
@@ -45,4 +66,4 @@ def summary():
     print('The Summary view will be added in this function')
 
 
-menu()
+add_stars_to_account(account_id='b7b68fb3-392d-11ed-b824-3c2c30ef82a0', add_count=3)
